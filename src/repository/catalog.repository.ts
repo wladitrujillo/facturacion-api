f393bff3f40e986745317bc78a63f81ee8f9a318
Wladimir Trujillo <wladimir.trujillo.ec@gmail.com>
2022-01-05 23:07:28 -0500

    merge changes

--- src/repository/catalog.repository.ts
+++ src/repository/catalog.repository.ts
@@ -1,10 +1,15 @@
-
 import { ICatalog, Catalog } from "../model/catalog";
 import RepositoryBase from "./base.repository";
 
 class CatalogRepository extends RepositoryBase<ICatalog> {
     constructor() {
         super(Catalog);
+    }
+    
+    retrieveWithCriteria(criteria: any) {
+
+        return Catalog.find(criteria);
+
     }
 }
 
