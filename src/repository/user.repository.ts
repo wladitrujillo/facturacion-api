e84d83146602fbecb6b3d5baa17704a9efa100a1
Wladimir Trujillo <wladimir.trujillo.ec@gmail.com>
2022-02-11 00:12:50 -0500

    add branch user

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,10 +1,23 @@
-import { Schema } from "mongoose";
+import { Schema, Types } from "mongoose";
 import { IUser, User } from "../model/user";
 import RepositoryBase from "./base.repository";
 
 class UserRepository extends RepositoryBase<IUser> {
     constructor() {
         super(User);
+    }
+
+    findById(_id: Types.ObjectId): Promise<IUser> {
+
+        return new Promise((resolve, reject) => {
+            User.findById(_id)
+                .populate("branch")
+                .exec((error: any, result: IUser) => {
+                    if (error) reject(error)
+                    else resolve(result);
+                });
+
+        });
     }
 }
 
